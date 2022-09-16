f91a10443bb3a1dee5c156c7d65c1c3cc0922cd9
“jms” <jstapleton536@gmail.com>
2022-09-16 19:16:46 -0400

    store.js class file

--- db/store.js
+++ db/store.js
@@ -5,33 +5,38 @@ constructor (id, title, text) {
     this.text = text;
 }
 read () {
-    returns db.json object
+    // returns db.json object
     return readFileAsync('db/db.json', 'utf8');
 }
-write()
-    return db/db.json JSON.stringify(note);
-    
+write() {
+return writeFileAsync('db/db.json', JSON.stringify(note));
+}
 
-getNotes()
-    return read().then(notes) 
-    parse notes
-    concat notes to JSON.parse(notes)
-
-addNotes()
-    you need to save title, text, 
+getNotes() {
+    return read().then((notes) => {
+        parsedNotes = JSON.parse(notes);
+    });
+}
+addNotes() {
+    // you need to save title, text, 
     const { title, text} = note
         if (!title || !text) {
-            throw error can't be null
-                 catch console.log (err)
+            console.log("Please fill out both fields");
         } 
-    add notes to db.json
+    // add notes to db.json
 
-    create varaible to hold our new note with the new given id
+    // create varaible to hold our new note with the new given id
     const userNote = {title, text, id} 
 
-    grab all notes and new notes and update notes to return the new note : use spread operator (...notes, userNote)
+    // grab all notes and new notes and update notes to return the new note : use spread operator (...notes, userNote)
+    const allNotes = [...notes, userNote];
 
-deleteNotes()
+}
+
+deleteNotes() {
+
+//optional
+}
 }
 
 module.export = Store;
