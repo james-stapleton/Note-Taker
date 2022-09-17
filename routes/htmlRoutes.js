4c2207a7b8d2036c5a0dae979570effb55a77bb9
“jms” <jstapleton536@gmail.com>
2022-09-16 20:06:49 -0400

    imports for the htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,14 @@
 //https://www.npmjs.com/package/uuid install AND REQUIRE this package (utils, fs, uuid)
 
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+console.log(uuidv4()); //random ID
+
 //this package will help with generating unique ids in the db.json file
 
 //create variables for readfileasync and writefile async
 
-//const writeFileAsync = something else with util
+const writeFileAsync = util.promisify(fs.writefile);
 
 
-//const readFileAsync = util.promisify(fs.readfile);
+const readFileAsync = util.promisify(fs.readfile);
