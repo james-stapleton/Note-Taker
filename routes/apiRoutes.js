76fc618ef4dd923f0c63e12d2c76b3a906d9a8fe
“jms” <jstapleton536@gmail.com>
2022-09-16 20:15:06 -0400

    router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,15 @@
 //require router and store
+const router = require("router");
+const store = require("Store");
 
 //get route to all notes from DB
-
 router.get('/notes', (request, response) => {
     store
     .getNotes()
     .then((notes) => response.json(notes));
 });
-//post route to add the notes
 
+//post route to add the notes
 router.post('/notes', (request, response) => {
     store
     .addNote(request.body) //anytime user takes input and puts to database, use request.body
@@ -16,5 +17,11 @@ router.post('/notes', (request, response) => {
 });
 
 //delete route to delete notes
+router.delete('/notes', (req, res) => {
+    store
+    .deleteNotes(req.body)
+    //! .then(()) 
+});
 
 //export my module
+module.exports = router;
